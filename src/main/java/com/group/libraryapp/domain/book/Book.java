5cdc73fdda8b6354293fd2bc65af19ad956b3354
jeongjaebong <rkqkdrn@naver.com>
2026-02-04 01:29:21 +0900

    spring boot 3.5.9 update

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -4,11 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
 
 @Getter
 @Entity
